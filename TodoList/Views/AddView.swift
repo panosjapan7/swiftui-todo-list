b548085b2a2fbb5772180cdafb66ee4f8e8276af
panosjapan7 <panosjapan@gmail.com>
2023-08-25 09:07:59 +0200

    1. Create a List of Todo items in SwiftUI

---
+++ TodoList/Views/AddView.swift
@@ -0,0 +1,45 @@
+//  AddView.swift
+
+import SwiftUI
+
+struct AddView: View {
+    
+    @State var textFieldText: String =  ""
+    
+    var body: some View {
+        ScrollView {
+            VStack {
+                TextField("Type something here...", text: $textFieldText)
+                    .padding(.horizontal)
+                    .frame(height: 55)
+                    .background(Color(#colorLiteral(red: 0.8374180198, green: 0.8374378085, blue: 0.8374271393, alpha: 1)))
+                    .cornerRadius(10)
+                
+                Button {
+                    
+                } label: {
+                    Text("Save".uppercased())
+                        .foregroundColor(.white)
+                        .font(.headline)
+                        .frame(height: 55)
+                        .frame(maxWidth: .infinity)
+                        .background(Color.accentColor)
+                        .cornerRadius(10)
+                }
+
+            }
+            .padding(14)
+            
+        }
+        .navigationTitle("Add an item 🖊️")
+    }
+}
+
+struct AddView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView{
+            AddView()
+        }
+        
+    }
+}
