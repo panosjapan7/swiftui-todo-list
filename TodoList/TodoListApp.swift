fd9857ea9843f0a0653835679fc6f39133eec7cb
panosjapan7 <panosjapan@gmail.com>
2023-08-25 08:33:24 +0200

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Panagiotis Tsapanidis on 2023-08-25.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
